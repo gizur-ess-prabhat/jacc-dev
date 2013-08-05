8827adffc63c09b18ca2b866cd882ac435cf821b
jonas colmsjo <jonas@gizur.com>
2013-08-05 18:20:21 +0200

    Fix

--- jacc.js
+++ jacc.js
@@ -80,7 +80,7 @@
     //======================================================================
 
     this._isset = function(a, message){
-      helpers.logDebug('_isset: checking - ' + a + ' with length - '+a.length() );
+      helpers.logDebug('_isset: checking - ' + a );
       if (a === "" || a === undefined || a === null || a === [] || a === {}) {
         helpers.logErr(message);
         process.exit();        
