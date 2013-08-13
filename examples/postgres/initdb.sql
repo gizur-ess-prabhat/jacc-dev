907485a8fd0096421ddf351d0c7798daae394760
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-08-13 16:42:26 +0200

    Fix

--- examples/postgres/initdb.sql
+++ examples/postgres/initdb.sql
@@ -1,4 +1,5 @@
 /* Create a user (same as role) named 'openerp' in Postgres with the password 'openerp'  */
+/* Run with psql -h <IP> -U postgres -f initdb.sql                                */
 
 CREATE ROLE openerp WITH SUPERUSER LOGIN PASSWORD 'openerp';
 
