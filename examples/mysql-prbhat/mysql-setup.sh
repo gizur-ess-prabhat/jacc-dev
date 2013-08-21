d555168541d0b960f98758a035714d5ba2d4bc4c
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-08-21 09:12:42 +0200

    Fix

--- examples/mysql-prbhat/mysql-setup.sh
+++ examples/mysql-prbhat/mysql-setup.sh
@@ -4,7 +4,9 @@
 dpkg-divert --local --rename --add /sbin/initctl
 ln -s /bin/true /sbin/initctl
 
-apt-get update && apt-get install -y mysql-server && apt-get clean && rm -rf /var/lib/apt/lists/*
+apt-get update
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server && apt-get clean && rm -rf /var/lib/apt/lists/*
 
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 
